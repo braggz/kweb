c52dd08892939ab85412d25451dd962cf38f0f67
braggz <root@localhost.localdomain>
2019-04-29 18:17:45 -0700

    Cleaned up code and made some small changes

--- kweb.js
+++ kweb.js
@@ -1,98 +1,99 @@
 #!/usr/bin/env node
 
-var mysql = require('mysql');
-const express = require('express');
+var mysql = require('mysql');	// All the Dependencies the program needs
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());onst express = require('express');
 const app = express();
 var cookieParser = require('cookie-parser');
 var bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 var fs = require('fs');
-var key = fs.readFileSync('encryption/server.key');
-var cert = fs.readFileSync( 'encryption/server.cert' );
+var key = fs.readFileSync('encryption/server.key'); //were the https key is located
+var cert = fs.readFileSync( 'encryption/server.cert' );//Where the server certificate is located
+const https = require('https')
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());	//Setting up the bodyparser and cookie parser to be used
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-var options = {
+
+var options = {		//Creates json object to store information about the https files
       key: key,
       cert: cert,
       
       };
-const https = require('https')
-app.set('view engine', 'pug');
-const server = app.listen(80, () => {
- console.log(`Express running → PORT ${server.address().port}`);
+const server = app.listen(80, () => { //Starts the http server
+ console.log('Http Connection');
 });
 
-var con = mysql.createConnection({
+https.createServer({key,cert}, app).listen(443, () => { //Starts the https server
+  console.log('Https Connection')
+})
+
+var con = mysql.createConnection({ // Connects to the https server, the password for this one is changed for protection
   host: "localhost",
   user: "kweb",
   password: "password",
   database: "mydb"
 });
-https.createServer({key,cert}, app).listen(443, () => {
-  console.log('Listening...')
-})
+
 app.get('/', (req, res) => {
-	if(req.secure){
+	if(req.secure){ 
 		
-	con.query("SELECT * FROM user WHERE username='"+req.cookies.userName+"'",function(err,result,fields){
+	con.query("SELECT * FROM user WHERE username='"+req.cookies.userName+"'",function(err,result,fields){ // Here i am using cookies to greet the user when they have logged in in the past
 	console.log(result + " " + req.cookies.userName);
 	if(result.length>0){res.render('index',{test:'Welcome Back '+req.cookies.userName});}
 	else{res.render('index');} 	
 	});
 	}
-	else{res.redirect('https://' + req.headers.host + req.url);}
+	else{res.redirect('https://' + req.headers.host + req.url);} //redirects the user to https if the user connects with http
 });
 
-app.post('/login', function(req, res) {	
+app.post('/login', function(req, res) {	//Checks the sql database to see if a users username and password is legitamate
   	
   	con.query("SELECT password FROM user WHERE username ='" +req.body.username+"'", function (err, result, fields) {
 	if(result.length>0){
     	
-   	var sqlResult = result[0].password;
+   	var sqlResult = result[0].password; 
 
 
 	if(req.body.password==sqlResult){
-	let users = {user:'test'}
-	res.cookie("userName",req.body.username);
+	
+	res.cookie("userName",req.body.username);//Adds a cookies to the users pc if they login successfully 
 	console.log(req.cookies);
 	res.render('./pugFiles/loginSuc');
 	
 	;}
 	else{res.render('index', {test:'Bad Password'});} 
   
-}	else{res.render('index',{test:'bad username'});}
+	}else{res.render('index',{test:'bad username'});}
 
-});
+	});
 	   
 });
+
 app.post('/createAcc', function(req,res){
 	res.render('./pugFiles/createAcc');
-	
-
-
 });
-app.post('/newAcc', function(req,res){
-		if(req.body.secret =='chicken'){
+
+app.post('/newAcc', function(req,res){//The page for users to create an account
+	if(req.body.secret =='chicken'){
   
-
-
 	con.query("SELECT username FROM user WHERE username = '"+req.body.username+"'",function(err,result,fields){
 	if(result.length <=0){
 
 	con.query("INSERT INTO user (user_id,username,password) VALUES (null,'"+req.body.username+"', '"+req.body.password+"' )" ,function (err, result) {
-    //	if (err) throw err;
+   
     	console.log("1 record inserted");
 		
 	res.render('index',{
-	test:'Account Created'}
-	);
+	test:'Account Created'});
+	});	
 
+	}else{res.render('./pugFiles/createAcc', {test:'Username Already Used'});}
 
-});	
-}else{res.render('./pugFiles/createAcc', {test:'Username Already Used'});}
-
-});
-}	else{res.render('./pugFiles/createAcc', {test:'Secret Code is Incorrect'});}
+	});
+	}else{res.render('./pugFiles/createAcc', {test:'Secret Code is Incorrect'});}
 
 });
 
