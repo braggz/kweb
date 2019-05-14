201858fabf3d90efcd6496538fb65e5be1cae48c
braggz <root@localhost.localdomain>
2019-05-14 14:33:19 -0700

    modified:   kweb.js 	modified:   package-lock.json 	modified:   package.json 	modified:   views/pugFiles/loginSuc.pug

--- kweb.js
+++ kweb.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
-const express = require('express');
-
+var keygen=require("keygenerator");
 var mysql = require('mysql');	// All the Dependencies the program needs
+var express = require('express');
+var ejs= require('ejs');
 const app = express();
 var cookieParser = require('cookie-parser');
 var bodyParser = require("body-parser");
 var fs = require('fs');
+var formidable = require('formidable');
 var key = fs.readFileSync('encryption/server.key'); //were the https key is located
 var cert = fs.readFileSync( 'encryption/server.cert' );//Where the server certificate is located
 const https = require('https')
-
+const multer = require('multer');
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +19,26 @@ app.use(cookieParser());
 app.use(bodyParser.json());	//Setting up the bodyparser and cookie parser to be used
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static('public'))
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    if (!fs.existsSync('/var/www/Kweb/uploads/'+req.cookies.userName)){
+	console.log('dir created');
+    fs.mkdirSync('/var/www/Kweb/uploads/'+req.cookies.userName);
+}
+	cb(null, '/var/www/Kweb/uploads/'+req.cookies.userName)
+  },
+  filename: function (req, file, cb) {
+    cb(null, req.cookies.userName+'.'+file.fieldname + '-' + Date.now())
+  }
+})
+
+var upload = multer({ storage: storage })
+ 
+
+
+ 
+
 
 var options = {		//Creates json object to store information about the https files
 	key: key,
@@ -39,17 +61,19 @@ var con = mysql.createConnection({ // Connects to the mySQL server, the password
 });
 
 app.get('/', (req, res) => {
+	console.log(keygen.session_id());
 	if(req.secure){ 
 		con.query("SELECT * FROM user WHERE username='"+req.cookies.userName+"'",function(err,result,fields){ // Here i am using cookies to greet the user when they have logged in in the past
 			if(result.length>0){
 				res.render('index',{test:'Welcome Back '+req.cookies.userName});
 			}
-			else{res.render('index');} 	
+			else{res.render('index');}		 	
 		});
 	}
 	else{
 		res.redirect('https://' + req.headers.host + req.url);
 	} //redirects the user to https if the user connects with http
+	
 });
 
 app.post('/login', function(req, res) {	//Checks the sql database to see if a users username and password is legitamate
@@ -58,8 +82,19 @@ app.post('/login', function(req, res) {	//Checks the sql database to see if a us
     			var sqlResult = result[0].password; 
 			if(req.body.password==sqlResult){
 				res.cookie("userName",req.body.username);//Adds a cookies to the users pc if they login successfully 
-				res.render('./pugFiles/loginSuc');
-	
+					con.query("SELECT isAdmin FROM user WHERE username ='" +req.body.username+"'", function (err, result, fields) {
+						if(result[0].isAdmin ==1){res.render('./pugFiles/loginSuc',)
+							console.log(result[0].isAdmin);
+			//				document.getElementById("adminDel").style.display='block';							
+						;}
+						else{res.render('./pugFiles/loginSuc',)
+						 console.log(result[0].isAdmin);
+			//			document.getElementById("adminDel").style.display='none';
+
+						;}
+				});
+			
+				
 			;}
 			else{
 				res.render('index', {test:'Bad Password'});
@@ -100,4 +135,31 @@ app.post('/newAcc', function(req,res){//The page for users to create an account
 
 app.post('/catPics',function(req,res){
 	res.render('./pugFiles/catPics');
+
+		var cat="";
+
+		for(var i =0;i<1;i++){
+		
+		cat+=("<p>test</p>");
+	
+	
+		}
+		
+		cat.toString();
+		
+
+		res.render('./pugFiles/catPics',{test:cat});
+			console.log(res.header.test);
+		//res.send(cat);
+			
+
+});
+app.post('/admin', upload.single('theFile'), (req, res) => {
+ res.render('index');
+});
+app.post('/delUsers', function(req, res) {
+	con.query("DELETE FROM user WHERE isAdmin IS NULL",function(err,result,fields){
+	console.log('All users deleted');
+	res.render('pugFiles/loginSuc');
+});
 });
