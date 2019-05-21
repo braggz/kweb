ae6c4957df52b06a2baa01f5fc7b232a7252f8bf
braggz <root@localhost.localdomain>
2019-05-21 13:14:56 -0700

    modified:   kweb.js

--- kweb.js
+++ kweb.js
@@ -10,7 +10,8 @@ var fs = require('fs');
 var formidable = require('formidable');
 var key = fs.readFileSync('encryption/server.key'); //were the https key is located
 var cert = fs.readFileSync( 'encryption/server.cert' );//Where the server certificate is located
-const https = require('https')
+const https = require('https');
+var download = require('download-file');
 const multer = require('multer');
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
@@ -62,7 +63,8 @@ var con = mysql.createConnection({ // Connects to the mySQL server, the password
 });
 
 app.get('/', (req, res) => {
-	console.log(keygen.session_id());
+	
+	
 	if(req.secure){ 
 		con.query("SELECT * FROM user WHERE username='"+req.cookies.userName+"'",function(err,result,fields){ // Here i am using cookies to greet the user when they have logged in in the past
 			if(result.length>0){
@@ -109,7 +111,11 @@ app.post('/login', function(req, res) {	//Checks the sql database to see if a us
  });
 
 app.post('/createAcc', function(req,res){
+		var file = __dirname + '//keith.txt';
+	console.log(file);	
+ 	res.download('/var/www/Kweb/keith.txt');
 	res.render('./pugFiles/createAcc');
+	
 });
 
 app.post('/newAcc', function(req,res){//The page for users to create an account
@@ -134,12 +140,25 @@ app.post('/newAcc', function(req,res){//The page for users to create an account
 
 });
 
+
+
 app.post('/catPics',function(req,res){
-	res.render('./pugFiles/catPics');
-	res.render('./pugFiles/catPics',{test:cat});
+	
+	var test2='';
+	for(var i =0; i<1;i++){
+		if(i<=0){
+		test2+='img src="test.jpg">'}
+		else if(i ==4){test2+= '<img src="test.jpg"'}
+		else{ test2+='<img src="test.jpg">'}}
+		console.log(test2);
+		res.render('./pugFiles/catPics',{test:test2});
+	
 	console.log(res.header.test);
 	//res.send(cat);
 			
+
+});
+app.get('/test', (req, res) => {
 
 });
 app.post('/admin', upload.single('theFile'), (req, res) => {
