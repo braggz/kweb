5d9fbc936c0a33b2c1189be397a34fc3579b5009
braggz <root@localhost.localdomain>
2019-09-23 11:30:58 -0700

    deleted:    June_odd-eyed-cat_cropped.jpg deleted:    encryption/server.cert 	deleted:    encryption/server.key 	deleted:    file.js 	deleted:    index.html 	deleted:    index.html.1 	deleted:    index.html.2 	deleted:    indexBACKUP.pug 	deleted:    jpg 	deleted:    keith 	deleted:    keith.txt 	modified:   kweb.js 	deleted:    kweb2.js 	deleted:    kwebBACKUP.js 	deleted:    kwebdata.txt 	deleted:    mysql-community-release-el7-5.noarch.rpm 	deleted:    node 	deleted:    pgdg-redhat-repo-latest.noarch.rpm 	modified:   readme.txt 	deleted:    server.cert 	deleted:    server.key 	deleted:    sql.js 	deleted:    srs.txt 	deleted:    test.jpg 	deleted:    test.js 	deleted:    uploads/jon/jon.theFile-1558655753683 	deleted:    uploads/keith/keith.theFile-1558653665057 	deleted:    uploads/keith/keith.theFile-1558655187664 	deleted:    uploads/keith/keith.theFile-1558655235543 	deleted:    uploads/keith/keith.theFile-1558655274082 	deleted:    uploads/keith/keith.theFile-1558655515893 	deleted:    uploads/keith/keith.theFile-1558655574886 	deleted:    uploads/keith/keith.theFile-1558655666486 	deleted:    uploads/keith/keith.theFile-1558655706160 	deleted:    uploads/keith/keith.theFile-1558656621252 	deleted:    uploads/keith/keith.theFile-1558656638341 	deleted:    uploads/keith/keith.theFile-1558656965896 	deleted:    views/dashboard.pug 	deleted:    views/error.pug 	deleted:    views/fail.pug 	deleted:    views/index 	modified:   views/index.pug 	deleted:    views/indexBACKUP.pug 	deleted:    views/keith 	deleted:    views/keith.theFile-1557284317613 	deleted:    views/pugFiles/catPics.pug 	modified:   views/pugFiles/createAcc.pug 	modified:   views/pugFiles/loginSuc.pug 	modified:   views/styles/style.css 	deleted:    views/success.pug 	deleted:    views/test.pug 	deleted:    views/unauthenticated.pug
    	create_acc.js
    	encryption/
    	index.js
    	login.js
    	old_kweb_versions/
    	seperate_js_files_template.js
    	start_server.js
    	start_sql.js
    	views/styles/index.css

--- kweb.js
+++ kweb.js
@@ -1,18 +1,10 @@
 #!/usr/bin/env node
-var keygen=require("keygenerator");
-var mysql = require('mysql');   // All the Dependencies the program needs
+
+//Dependencies
 var express = require('express');
-var ejs= require('ejs');
 const app = express();
 var cookieParser = require('cookie-parser');
 var bodyParser = require("body-parser");
-var fs = require('fs');
-var formidable = require('formidable');
-var key = fs.readFileSync('encryption/server.key'); //were the https key is located
-var cert = fs.readFileSync( 'encryption/server.cert' );//Where the server certificate is located
-const https = require('https');
-var download = require('download-file');
-const multer = require('multer');
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,149 +13,42 @@ app.use(bodyParser.json());     //Setting up the bodyparser and cookie parser to
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static('public'))
-var fileName="test";
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    if (!fs.existsSync('/var/www/Kweb/uploads/'+req.cookies.userName)){
-        console.log('dir created');
-    fs.mkdirSync('/var/www/Kweb/uploads/'+req.cookies.userName);
-}
-        cb(null, '/var/www/Kweb/uploads/'+req.cookies.userName)
-  },
-  filename: function (req, file, cb) {
-        let extArray = file.mimetype.split("/");
-        let extension = extArray[extArray.length - 1];
-        fileName=req.cookies.userName+'.'+Date.now()+'.'+extension;
-        cb(null, req.cookies.userName+'.'+Date.now()+'.'+extension)
-        }
-})
-var upload = multer({ storage: storage })
+//My own functions
+var startServer = require('./start_server.js');
+var startSql = require('./start_sql.js');
+var index = require('./index.js');
+var login = require('./login.js');
+var createAcc = require('./create_acc.js');
+startServer.startServer(app);//Starts the server with a seperate function
+var con = startSql.startSql();//Connects to the sql server
 
-var options = {		//Creates json object to store information about the https files
-	key: key,
-	cert: cert,
-      
-};
-const server = app.listen(80, () => { //Starts the http server
-	console.log('Http Connection');
-});
-
-https.createServer({key,cert}, app).listen(443, () => { //Starts the https server
-	console.log('Https Connection')
-})
-
-var con = mysql.createConnection({ // Connects to the mySQL server, the password for this one is changed for protection
-	host: "localhost",
-  	user: "kweb",
-  	password: "password",
-  	database: "mydb"
-});
+var indexInfo;//The variable that stores text to display on index screen
+var displayCookie=false;
 
 app.get('/', (req, res) => {
-	
-	if(req.secure){ 
-		con.query("SELECT * FROM user WHERE username='"+req.cookies.userName+"'",function(err,result,fields){ // Here i am using cookies to greet the user when they have logged in in the past
-			if(result.length>0){
-				res.render('index',{test:'Welcome Back '+req.cookies.userName});
-			}
-			else{res.render('index');}		 	
-		});
-	}
-	else{
-		res.redirect('https://' + req.headers.host + req.url);
-	} //redirects the user to https if the user connects with http
-	
-});
-
-app.post('/login', function(req, res) {	//Checks the sql database to see if a users username and password is legitamate
-  	con.query("SELECT password FROM user WHERE username ='" +req.body.username+"'", function (err, result, fields) {
-		if(result.length>0){
-    			var sqlResult = result[0].password; 
-			if(req.body.password==sqlResult){
-				res.cookie("userName",req.body.username);//Adds a cookies to the users pc if they login successfully 
-					con.query("SELECT isAdmin FROM user WHERE username ='" +req.body.username+"'", function (err, result, fields) {
-						con.query("SELECT cName FROM file WHERE username ='" +req.body.username+"'", function (err, result, fields) {
-							if(result.length != null){
-								res.render('./pugFiles/loginSuc',{fileNames:result})
-							}
-							else{ res.render('./pugFiles/loginSuc',{fileNames:0})}
-							});
-					});
-			}
-			else{
-				res.render('index', {test:'Bad Password'});
-			} 
-  
-		}
-		else{
-			res.render('index',{test:'bad username'});
-		}	
-	});
+   if(!displayCookie && req.cookies['username']){indexInfo='Welcome Back '+req.cookies['username'];}
+   else if (!displayCookie){indexInfo='';}
+   index.index(req,res,con,indexInfo);
+   displayCookie=false;
  });
 
-app.post('/createAcc', function(req,res){
-	var file = __dirname + '//keith.txt';	
- 	res.download('/var/www/Kweb/keith.txt');
-	res.render('./pugFiles/createAcc');
-	
+app.post('/login', function(req, res) {	//Checks the sql database to see if a users username and password is legitamate
+  login.login(req,res,con, function(result){
+    indexInfo = result;
+    displayCookie=true;
+    res.redirect('/');
+  });
 });
 
-app.post('/newAcc', function(req,res){//The page for users to create an account
-	if(req.body.secret =='chicken'){
-  		con.query("SELECT username FROM user WHERE username = '"+req.body.username+"'",function(err,result,fields){
-			if(result.length <=0){
-				con.query("INSERT INTO user (user_id,username,password) VALUES (null,'"+req.body.username+"', '"+req.body.password+"' )" ,function (err, result) {
-   					console.log("1 record inserted");
-					res.render('index',{test:'Account Created'});
-				});
-	
-			}
-			else{
-				res.render('./pugFiles/createAcc', {test:'Username Already Used'});
-			}
-
-		});
-	}
-	else{
-			res.render('./pugFiles/createAcc', {test:'Secret Code is Incorrect'});
-	}
-
+app.get('/createAcc', function(req,res){//Renders the Create Account Screen
+  res.render('./pugFiles/createAcc');
 });
 
-app.post('/catPics',function(req,res){
-	var test;
-	for(var i=0; i<5;i++){
-		test.push('test.jpg');
-	}	
-	res.render('pugFiles/catPics.pug',{test:test});			
-});
-
-app.get('/downloadFile', (req, res) => {
-	con.query("SELECT path,name FROM file WHERE cName = '"+req.query.downloadBut+"'",function(err,result,fields){
-		path = result[0].path;
-		name = result[0].name; 
-		res.download(path+'/'+name);	
-        	});
-});
-
-app.get('/deleteFile',(req,res) => {
-	con.query("SELECT path,name FROM file WHERE cName = '"+req.query.delBut+"'",function(err,result,fields){
-		console.log(result[0].path+'/'+result[0].name);
-		fs.unlink(result[0].path+'/'+result[0].name,function(err){
-		console.log('File Deleted');
-		});		
-	});
-	con.query("DELETE FROM file WHERE cName = '"+req.query.delBut+"'",function(err,result,fields){	
-		res.redirect('/');
-	});
-});
-	
-app.post('/admin', upload.single('theFile'), (req, res) => {
-	
-	con.query("INSERT INTO file (username,name,path,cName) VALUES ('"+req.cookies.userName+"', '"+fileName+"','/var/www/Kweb/uploads/"+req.cookies.userName+"','"+req.body.fileName+"')" ,function (err, result) {
-		if(err){console.log(err);}	
-		console.log('file logged');
-});
-	res.redirect('/');
+app.post('/createAcc', function(req,res){//Chechs that a user properly fills out forms and then inserts it into the database
+  createAcc.createAcc(req,res,con, function(result){
+	indexInfo=result;
+        displayCookie=true;	
+	res.redirect('/');	
+  });
 });
--- readme.txt
+++ readme.txt
@@ -1,10 +1,1 @@
-This is a simple project for my school, it should not be used for anything 
-profesonal. 
-New Changes(4/28/19):
-Added https encryption
-Added cookies 
-Added MySQL database for Users password/username
-Added Cookies to keep track of user data
-Created a system for users to login and create accounts
-
 
